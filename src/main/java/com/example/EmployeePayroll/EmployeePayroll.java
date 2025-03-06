e09920d7a5b87fdad3e37b603a6e602ce294f5a4
Divyansh <divyanshbansal56@gmail.com>
2025-03-06 11:16:18 +0530

    [Divyansh Bansal] Added CRUD Operations

---
+++ src/main/java/com/example/EmployeePayroll/EmployeePayroll.java
@@ -0,0 +1,13 @@
+package com.example.EmployeePayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayroll {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayroll.class, args);
+	}
+
+}
